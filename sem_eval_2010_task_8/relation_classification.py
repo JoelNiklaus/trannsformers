cf1899ee2058141cfc3489d0355af49c78613487
Joel Niklaus <me@joelniklaus.ch>
2020-11-26 10:08:03 +0100

    made some smaller changes

--- sem_eval_2010_task_8/relation_classification.py
+++ sem_eval_2010_task_8/relation_classification.py
@@ -3,7 +3,7 @@ import random
 import fire
 import wandb
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3"  # do this to remove gpu with full memory (MUST be before torch import)
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"  # do this to remove gpu with full memory (MUST be before torch import)
 os.environ["TOKENIZERS_PARALLELISM"] = "true"  # used for disabling warning (BUT: if deadlock occurs, remove this)
 
 from transformers import EvaluationStrategy, AutoModelForSequenceClassification, AutoTokenizer
@@ -16,19 +16,26 @@ from util import make_reproducible, compute_metrics, get_prediction_ids
 from pprint import pprint
 
 
-def run(base_model="roberta-base", fine_tuned_checkpoint_name=None, dataset="joelito/sem_eval_2010_task_8",
-        num_train_epochs=10, do_train=False, do_eval=False, do_predict=True, test_set_sub_size=None, seed=42):
+def run_multiple_seeds(num_seeds=10):
+    for seed in range(num_seeds):
+        run(seed=seed)
+
+
+def run(base_model="roberta-base", fine_tuned_checkpoint=None, dataset="joelito/sem_eval_2010_task_8",
+        num_train_epochs=3, do_train=False, do_eval=False, do_predict=True, batch_size=14, test_set_sub_size=10,
+        seed=42):
     """
     Runs the specified transformer model
     :param base_model:             the name of the base model from huggingface transformers (e.g. roberta-base)
-    :param fine_tuned_checkpoint_name:  the name of the fine tuned checkpoint (e.g. checkpoint-500)
+    :param fine_tuned_checkpoint:  the name of the fine tuned checkpoint (e.g. checkpoint-500)
     :param dataset:                the name of the dataset from huggingface datasets (e.g. joelito/sem_eval_2010_task_8)
-    :param num_train_epochs:            number of epochs to train for
-    :param do_train:                    whether to train the model
-    :param do_eval:                     whether to evaluate the model in the end
-    :param do_predict:                  whether to do predictions on the test set in the end
-    :param test_set_sub_size:           make faster by only selecting small subset, otherwise just set to False/None
-    :param seed:                        random seed for reproducibility
+    :param num_train_epochs:       number of epochs to train for
+    :param do_train:               whether to train the model
+    :param do_eval:                whether to evaluate the model in the end
+    :param do_predict:             whether to do predictions on the test set in the end
+    :param batch_size:             size of the batch (dependent on gpu: e.g. bert-base => 6, distilbert-base => 14 on 8GB)
+    :param test_set_sub_size:      make faster by only selecting small subset, otherwise just set to False/None
+    :param seed:                   random seed for reproducibility
     :return:
     """
 
@@ -37,20 +44,20 @@ def run(base_model="roberta-base", fine_tuned_checkpoint_name=None, dataset="joe
 
     make_reproducible(seed)
     training_args = TrainingArguments(
-        output_dir=f'{local_model_name}/results',  # output directory
+        output_dir=f'{local_model_name}/results/{seed}',  # output directory
         num_train_epochs=num_train_epochs,  # total number of training epochs
         max_steps=-1,  # Set to a small positive number to test models (training is short)
-        per_device_train_batch_size=6,  # batch size per device during training
-        per_device_eval_batch_size=6,  # batch size for evaluation
-        warmup_steps=500,  # number of warmup steps for learning rate scheduler
+        per_device_train_batch_size=batch_size,  # batch size per device during training
+        per_device_eval_batch_size=batch_size,  # batch size for evaluation
+        warmup_steps=100,  # number of warmup steps for learning rate scheduler
         weight_decay=0.01,  # strength of weight decay
-        logging_dir=f'{local_model_name}/logs',  # directory for storing logs
+        logging_dir=f'{local_model_name}/logs/{seed}',  # directory for storing logs
         logging_steps=10,
         save_steps=500,
         eval_steps=100,
         evaluation_strategy=EvaluationStrategy.STEPS,
         seed=seed,
-        run_name=base_model,  # used for wandb
+        run_name=f"{base_model}/{seed}",  # used for wandb
         load_best_model_at_end=True,
         metric_for_best_model="f1",
         greater_is_better=True,
@@ -69,8 +76,8 @@ def run(base_model="roberta-base", fine_tuned_checkpoint_name=None, dataset="joe
     label2id = {v: k for k, v in enumerate(idx_to_labels_list)}
 
     model_path = base_model
-    if fine_tuned_checkpoint_name:
-        model_path = f"{training_args.output_dir}/{fine_tuned_checkpoint_name}"
+    if fine_tuned_checkpoint:
+        model_path = f"{training_args.output_dir}/{fine_tuned_checkpoint}"
 
     print("Loading Model")
     model = AutoModelForSequenceClassification.from_pretrained(model_path, id2label=id2label, label2id=label2id,
