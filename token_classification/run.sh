cda1589a9365ca6ad167f681824ecb282c1b62c0
Joel Niklaus <me@joelniklaus.ch>
2020-11-27 16:50:45 +0100

    adapted run script

--- token_classification/run.sh
+++ token_classification/run.sh
@@ -1,9 +1,10 @@
 
 python3 run_ner.py \
-  --model_name_or_path distilbert-base-uncased \
+  --model_name_or_path distilbert-base-german-cased \
   --dataset_name joelito/ler \
-  --output_dir ./ler \
+  --output_dir ./ler/distilbert-base-german-cased \
   --do_train \
   --do_eval \
   --do_predict \
-  --per_device_train_batch_size 14
+  --seed 42 \
+  --per_device_train_batch_size 12
